798269c4e9553a01200f07c5088932a88e5a84e7
ThaAlony <longo@DESKTOP-TLO6VTA.homenet.telecomitalia.it>
2021-05-06 12:47:21 +0200

    Classe Persona ( abstract)

--- src/main/java/it/edu/isspitagora/esercizio/Persona.java
+++ src/main/java/it/edu/isspitagora/esercizio/Persona.java
@@ -9,6 +9,58 @@ package it.edu.isspitagora.esercizio;
  *
  * @author alony
  */
-public class Persona {
+public abstract class Persona {
+    
+    static int codici;
+    private int codice;
+    private String cognome;
+    private String nome;
+    private int anno;
+    private final int ANNOATTUALE = 2021;
+    
+    public Persona(String nome, String cognome, int anno) {
+        this.codici++;
+        this.codice = this.codici;
+        this.anno = anno;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public void setCodice(int codice) {
+        this.codice = codice;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+    
+    @Override
+    public Persona clone() {
+        return null;
+    }
     
 }
